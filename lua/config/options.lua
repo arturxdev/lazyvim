3b413d70d9123188dec98783211c15f9e729c7cf
Arturo Guerrero Lopez <aguerrero@wiggi.mx>
2025-01-15 10:34:38 -0600

    add cheetshets

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,7 @@
 local opt = vim.opt
 
 opt.relativenumber = false
+-- Disable cursorline and relativenumber during scrolling
+vim.o.lazyredraw = true -- Skip redraw during scrolling
+vim.o.ttyfast = true -- Faster terminal rendering
+vim.o.termguicolors = true -- Ensure true color support
